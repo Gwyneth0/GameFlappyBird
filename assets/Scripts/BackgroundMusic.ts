837a24e8cdced1612f41eda8fae9bfa3f6708be6
Gwyneth0 <hieulu8902@gmail.com>
2023-05-19 18:05:00 +0700

    buildnew

---
+++ assets/Scripts/BackgroundMusic.ts
@@ -0,0 +1,21 @@
+// import { _decorator, AudioClip, AudioSource, Component, Node } from 'cc';
+// const { ccclass, property } = _decorator;
+
+// @ccclass('BackgroundMusic')
+// export class BackgroundMusic extends Component {
+//     @property({ type: AudioSource })
+//     private audioSource: AudioSource = null!;
+
+//     protected onLoad() {
+//         // Cấu hình các thuộc tính âm thanh
+//         this.audioSource.clip = this.loadAudio('path/to/your/audio'); // Đường dẫn đến tệp âm thanh
+//         this.audioSource.loop = true; // Lặp lại nhạc nền
+//         this.audioSource.play(); // Phát nhạc nền
+//     }
+
+//     private loadAudio(path: string): AudioClip {
+//         // Logic để tải tệp âm thanh
+//         // Trả về một đối tượng AudioClip từ tệp âm thanh được tải
+//     }
+// }
+
--- assets/Scripts/BirdAudio.ts
+++ assets/Scripts/BirdAudio.ts
@@ -3,25 +3,12 @@ const { ccclass, property } = _decorator;
 
 @ccclass("BirdAudio")
 export class BirdAudio extends Component {
-  @property({
-    type: [AudioClip],
-    tooltip: "place audio clip here",
-  })
+
+  @property([Audio])
   private clips: AudioClip[] = [];
-  @property({
-    type: AudioSource,
-    tooltip: "place audio node here",
-  })
-  private AudioSource: AudioSource;
 
   @property(AudioSource)
   private audioSource: AudioSource = null!;
-  
-  @property({
-    type: Button,
-    tooltip: 'Sound Button'
-  })
-  private soundButton: Button;
 
   @property(Node)
   private Mute: Node;
@@ -29,18 +16,18 @@ export class BirdAudio extends Component {
   @property(Node)
   private unMute: Node;
   
-  onAudioQueue(index: number) {
+  public onAudioQueue(index: number) {
     let clip: AudioClip = this.clips[index];
     this.audioSource.playOneShot(clip);
   }
   // turn off / turn on mute
-  onSoundButtonClick() {
+  protected onSoundButtonClick() {
     this.audioSource.volume = 1;
     this.unMute.active = false;
     this.Mute.active = true;
   
   }
-  offSoundButtonClick() {
+  protected offSoundButtonClick() {
     this.audioSource.volume = 0;
     this.Mute.active = false;
     this.unMute.active = true;
