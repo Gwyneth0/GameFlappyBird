823152c98c5500c53bb500eee3d20c84ffb65e6e
Gwyneth0 <hieulu8902@gmail.com>
2023-05-23 14:40:18 +0700

    update code

--- assets/Scripts/PipePool.ts
+++ assets/Scripts/PipePool.ts
@@ -1,6 +1,4 @@
 import { _decorator, Component, Node, Prefab, NodePool, instantiate, math } from 'cc';
-import { Results } from './Results';
-import { Bird } from './Bird';
 const { ccclass, property } = _decorator;
 
 @ccclass('PipePool')
@@ -9,27 +7,13 @@ export class PipePool extends Component {
     @property(Prefab)
     private prefabPipes: Prefab = null;
 
-    @property(Node)
+    @property(Node)S
     private pipePoolHome: Node = null;
 
     private pool: NodePool = new NodePool();
     private createPipe: Node = null;
 
     pipeNode: Node = null;
-
-    // pipe: Node[] = [null, null, null];
-
-    // @property({
-    //     type: Results,
-    //     tooltip: 'Result'
-    // })
-    // public result: Results;
-
-    // @property({
-    //     type: Bird,
-    //     tooltip: 'Bird'
-    // })
-    // public BirdCtr: Bird
 
     //Initialize the object pool with initial pipes
     protected initPool(): void {
@@ -59,52 +43,5 @@ export class PipePool extends Component {
         this.pool.clear();
         this.initPool();
     }
-
-    // protected createPipes(): void {
-    //     for (let i = 0; i < this.pipe.length; i++) {
-
-    //         this.pipe[i] = instantiate(this.prefabPipes);
-    //         this.pipeNode.addChild(this.pipe[i]);
-
-    //         var posX = this.pipe[i].position.x;
-    //         var posY = this.pipe[i].position.y;
-
-    //         posX = 500 + (400 * i);
-
-    //         var minY = -360;
-    //         var maxY = -720;
-
-    //         posY = math.randomRangeInt(minY, maxY);
-
-    //         this.pipe[i].setPosition(posX, posY, 0);
-    //     }
-    // }
-
-    // protected movePipes(): void {
-    //     for (let i = 0; i < this.pipe.length; i++) {
-
-    //         var posX = this.pipe[i].position.x;
-    //         var posY = this.pipe[i].position.y;
-
-    //         posX -= 1.0;
-
-    //         //check pass pipe, add score
-    //         var posBird = this.BirdCtr.node.position.x
-
-    //         if (posX === posBird) {
-    //             this.result.addScore();
-    //         }
-    //         if (posX <= -610) {
-    //             posX = 520;
-
-    //             var minY = -360;    //old: -360
-    //             var maxY = -720;    //old: -600
-
-    //             posY = math.randomRangeInt(minY, maxY)
-    //         }
-
-    //         this.pipe[i].setPosition(posX, posY, 0);
-    //     }
-    // }
 
 }
